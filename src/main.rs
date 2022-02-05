a457a5feaaadbb6a13f632de4066a1d6458e6687
Danilo Bargen <mail@dbrgn.ch>
2022-02-05 14:19:23 +0100

    Store file in file system

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,23 @@
-use std::{borrow::Cow, net::SocketAddr};
+use std::{borrow::Cow, net::SocketAddr, path::PathBuf, process, sync::Arc};
 
 use axum::{
-    body::{boxed, Full},
-    extract::{ContentLengthLimit, Multipart},
+    body::{boxed, Bytes, Full},
+    extract::{ContentLengthLimit, Extension, Multipart},
     http::{header, Response},
-    response::{Html, IntoResponse},
+    response::{Html, IntoResponse, Redirect},
     routing::get,
-    Router,
+    AddExtensionLayer, Router,
 };
+use clap::Parser;
 use rust_embed::RustEmbed;
+use tracing::{error, info};
+
+#[derive(Parser)]
+struct Args {
+    /// Path to the directory where files will be put.
+    #[clap(short, long)]
+    target_dir: PathBuf,
+}
 
 #[derive(RustEmbed)]
 #[folder = "public/"]
@@ -18,14 +27,27 @@ struct EmbeddableStaticFile;
 async fn main() {
     // Set the RUST_LOG, if it hasn't been explicitly defined
     if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "webdropper=debug,tower_http=debug")
+        std::env::set_var("RUST_LOG", "webdropper=debug,tower_http=info")
     }
     tracing_subscriber::fmt::init();
 
-    // build our application with some routes
+    // Parse args
+    let args = Args::parse();
+    if !args.target_dir.exists() {
+        eprintln!("Error: Target dir {:?} does not exist", args.target_dir);
+        process::exit(1);
+    }
+    if !args.target_dir.is_dir() {
+        eprintln!("Error: Path {:?} is not a directory", args.target_dir);
+        process::exit(1);
+    }
+    let args = Arc::new(args);
+
+    // Build our application with some routes
     let app = Router::new()
         .route("/", get(index).post(accept_form))
         .route("/scripts.js", get(scripts))
+        .layer(AddExtensionLayer::new(args))
         .layer(tower_http::trace::TraceLayer::new_for_http());
 
     // run it with hyper
@@ -57,11 +79,27 @@ type LimitedMultipart = ContentLengthLimit<
     },
 >;
 
-async fn accept_form(ContentLengthLimit(mut multipart): LimitedMultipart) {
-    while let Some(field) = multipart.next_field().await.unwrap() {
-        let name = field.name().unwrap().to_string();
-        let data = field.bytes().await.unwrap();
+async fn store_file(filename: &str, bytes: Bytes, args: &Args) -> std::io::Result<()> {
+    info!(
+        "Store file with filename {:?} and {} bytes to dir {:?}",
+        filename,
+        bytes.len(),
+        &args.target_dir
+    );
+    tokio::fs::write(args.target_dir.join(filename), bytes).await?;
+    Ok(())
+}
 
-        println!("Length of `{}` is {} bytes", name, data.len());
+async fn accept_form(
+    ContentLengthLimit(mut multipart): LimitedMultipart,
+    Extension(args): Extension<Arc<Args>>,
+) -> Redirect {
+    // Store all files in the multipart body in the file system
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        let file_name = field.file_name().unwrap().to_string();
+        if let Err(e) = store_file(&file_name, field.bytes().await.unwrap(), &args).await {
+            error!("Failed to upload file {}: {:?}", file_name, e);
+        }
     }
+    Redirect::to("/".parse().unwrap())
 }
