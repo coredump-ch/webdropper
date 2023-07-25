aa411a39d2ec1f927d9a4a38ada4caeac24a43d9
Raphael Nestler <raphael.nestler@gmail.com>
2023-07-25 13:37:02 +0200

    Execute JS after DOMContentLoaded
    That way we avoid redeclaring global constants, since they are only
    local to the event handler.
    
    Also since we need to reattach all handlers this fixes the document
    reload case.

--- public/scripts.js
+++ public/scripts.js
@@ -1,115 +1,117 @@
-// DOM elements
-const elements = {
-    wrapper: document.querySelector('#wrapper'),
-    uploadProgressWrapper: document.querySelector('#upload-progress-wrapper'),
-    uploadProgress: document.querySelector('#upload-progress'),
-    uploadForm: document.querySelector('#upload-form'),
-    uploadStatus: document.querySelector('#upload-status'),
-};
+addEventListener("DOMContentLoaded", (event) => {
+    // DOM elements
+    const elements = {
+        wrapper: document.querySelector('#wrapper'),
+        uploadProgressWrapper: document.querySelector('#upload-progress-wrapper'),
+        uploadProgress: document.querySelector('#upload-progress'),
+        uploadForm: document.querySelector('#upload-form'),
+        uploadStatus: document.querySelector('#upload-status'),
+    };
 
-/**
- * Generate random string.
- */
-const random = (length = 8) => {
-    // Declare all characters
-    let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    /**
+     * Generate random string.
+     */
+    const random = (length = 8) => {
+        // Declare all characters
+        let chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
 
-    // Pick characers randomly
-    let str = '';
-    for (let i = 0; i < length; i++) {
-        str += chars.charAt(Math.floor(Math.random() * chars.length));
+        // Pick characers randomly
+        let str = '';
+        for (let i = 0; i < length; i++) {
+            str += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+
+        return str;
+    };
+
+    const uploadStart = (max) => {
+        elements.uploadStatus.style.display = 'none';
+        elements.uploadProgressWrapper.style.display = 'block';
+        elements.uploadProgress.max = max;
+        elements.uploadProgress.value = 0;
     }
 
-    return str;
-};
+    const uploadProgress = (progress) => {
+        elements.uploadProgress.value = progress;
+    }
 
-const uploadStart = (max) => {
-    elements.uploadStatus.style.display = 'none';
-    elements.uploadProgressWrapper.style.display = 'block';
-    elements.uploadProgress.max = max;
-    elements.uploadProgress.value = 0;
-}
+    const uploadDone = () => {
+        elements.uploadProgressWrapper.style.display = 'none';
+    }
 
-const uploadProgress = (progress) => {
-    elements.uploadProgress.value = progress;
-}
+    const uploadFailed = (event) => {
+        elements.uploadProgressWrapper.style.display = 'none';
+        elements.uploadStatus.innerHTML = `Upload failed: #{event.type}`
+        elements.uploadStatus.style.display = 'block';
+    }
 
-const uploadDone = () => {
-    elements.uploadProgressWrapper.style.display = 'none';
-}
+    // Drag & drop
 
-const uploadFailed = (event) => {
-    elements.uploadProgressWrapper.style.display = 'none';
-    elements.uploadStatus.innerHTML = `Upload failed: #{event.type}`
-    elements.uploadStatus.style.display = 'block';
-}
+    const handleDragOver = (event) => {
+        event.preventDefault();
+        elements.wrapper.classList.add('drag-over');
+    }
+    const handleDragEnd = (event) => {
+        elements.wrapper.classList.remove('drag-over');
+    }
+    document.addEventListener('dragenter', handleDragOver);
+    document.addEventListener('dragover', handleDragOver);
+    document.addEventListener('dragleave', handleDragEnd);
 
-// Drag & drop
+    document.addEventListener('drop', (event) => {
+        event.preventDefault();
+        elements.wrapper.className = '';
 
-const handleDragOver = (event) => {
-    event.preventDefault();
-    elements.wrapper.classList.add('drag-over');
-}
-const handleDragEnd = (event) => {
-    elements.wrapper.classList.remove('drag-over');
-}
-document.addEventListener('dragenter', handleDragOver);
-document.addEventListener('dragover', handleDragOver);
-document.addEventListener('dragleave', handleDragEnd);
+        // Add files to form
+        const fileInput = document.querySelector('input[name="file"]');
+        fileInput.files = event.dataTransfer.files;
 
-document.addEventListener('drop', (event) => {
-    event.preventDefault();
-    elements.wrapper.className = '';
+        // Submit form
+        fileInput.closest('form').requestSubmit();
+    });
 
-    // Add files to form
-    const fileInput = document.querySelector('input[name="file"]');
-    fileInput.files = event.dataTransfer.files;
+    elements.uploadForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        console.log("Submit event:");
+        console.log(event);
 
-    // Submit form
-    fileInput.closest('form').requestSubmit();
-});
+        var xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener('loadstart', (event) => {
+            uploadStart(event.total);
+        }, false);
+        xhr.upload.addEventListener('progress', (event) => {
+            uploadProgress(event.loaded);
+        }, false);
+        xhr.upload.addEventListener('load', (event) => {
+            uploadProgress(event.total);
+        }, false);
 
-elements.uploadForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    console.log("Submit event:");
-    console.log(event);
-
-    var xhr = new XMLHttpRequest();
-    xhr.upload.addEventListener('loadstart', (event) => {
-        uploadStart(event.total);
-    }, false);
-    xhr.upload.addEventListener('progress', (event) => {
-        uploadProgress(event.loaded);
-    }, false);
-    xhr.upload.addEventListener('load', (event) => {
-        uploadProgress(event.total);
-    }, false);
-
-    xhr.addEventListener('readystatechange', (event) => {
-        console.log(event.target.readyState);
-        if (event.target.readyState === XMLHttpRequest.DONE) {
-            const status = event.target.status;
-            if (status === 0 || (status >= 200 && status < 400) && event.target.responseText) {
-                console.log('readystatechange', event);
-                //we got a response from the server and we're replacing the whole current document content with it, simulating a page reload
-                var newDocument = document.open();
-                newDocument.write(event.target.responseText);
-                newDocument.close();
-            } else {
-                console.error(event);
-                uploadFailed(event);
-                //throw new Error('Error in the response.');
+        xhr.addEventListener('readystatechange', (event) => {
+            console.log(event.target.readyState);
+            if (event.target.readyState === XMLHttpRequest.DONE) {
+                const status = event.target.status;
+                if (status === 0 || (status >= 200 && status < 400) && event.target.responseText) {
+                    console.log('readystatechange', event);
+                    //we got a response from the server and we're replacing the whole current document content with it, simulating a page reload
+                    var newDocument = document.open();
+                    newDocument.write(event.target.responseText);
+                    newDocument.close();
+                } else {
+                    console.error(event);
+                    uploadFailed(event);
+                    //throw new Error('Error in the response.');
+                }
             }
-        }
-    }, false);
+        }, false);
 
-    xhr.upload.addEventListener("error", uploadFailed);
-    xhr.upload.addEventListener("abort", uploadFailed);
-    xhr.upload.addEventListener("timeout", uploadFailed);
+        xhr.upload.addEventListener("error", uploadFailed);
+        xhr.upload.addEventListener("abort", uploadFailed);
+        xhr.upload.addEventListener("timeout", uploadFailed);
 
-    console.log(elements.uploadForm);
-    var openResult = xhr.open(elements.uploadForm.getAttribute('method'), elements.uploadForm.getAttribute('action'), true);
-    console.log('openResult:', openResult)
-    var sendResult = xhr.send(new FormData(elements.uploadForm));
-    console.log('sendResult:', sendResult)
+        console.log(elements.uploadForm);
+        var openResult = xhr.open(elements.uploadForm.getAttribute('method'), elements.uploadForm.getAttribute('action'), true);
+        console.log('openResult:', openResult)
+        var sendResult = xhr.send(new FormData(elements.uploadForm));
+        console.log('sendResult:', sendResult)
+    });
 });
